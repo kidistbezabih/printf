20bde8372c52f3698bd7c676f027f4f23aeeb9ae
Haylemicheal Berihun <haylemicheal.berihun@dventus.com>
2022-04-19 22:54:20 +0300

    Add print binary

--- main.h
+++ main.h
@@ -11,6 +11,11 @@ int _printf(const char *format, ...);
 int print_char(va_list arg);
 int print_str(va_list arg);
 int print_percent(va_list);
+int print_binary(va_list);
+unsigned int base_len(unsigned int, int);
+char *rev_string(char *);
+void write_base(char *str);
+char *_memcpy(char *dest, char *src, unsigned int n);
 
 /**
  * struct identifierStruct - structure definition of a printTypeStruct
