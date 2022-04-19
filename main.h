97fe99747270f5a46cc79db5eb83e699f20c14e1
kidistbezabih <kidistbezabh@gmail.com>
2022-04-19 20:12:51 +0300

    printf

--- main.h
+++ main.h
@@ -5,4 +5,13 @@ int _putchar(char c);
 int print_int(va_list arg);
 int print_unsigned(va_list arg);
 int _printf(const char *format, ...);
+
+
+typedef struct identifierStruct
+{
+char *identifier;
+int (*printer)(va_list);
+} identifierStruct;
+
+
 #endif
